28d38a9ff6b24573aeaa8daca3c217fa66bd17ff
Bruno Freijomil <bruno_freijomil@hotmail.com>
2025-08-24 16:49:26 -0300

    se agrage vibracion test

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Haptics, ImpactStyle } from '@capacitor/haptics';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent]
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton]
 })
 export class Tab2Page {
 
-  constructor() {}
-
+  async vibrar(tipo: 'corta' | 'larga') {
+    switch (tipo) {
+      case 'larga':
+        await Haptics.vibrate({ duration: 1500 });
+        break;
+      case 'corta':
+        await Haptics.impact({ style: ImpactStyle.Heavy });
+        break;
+    }
+  }
 }
